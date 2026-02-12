72f5ccabc2bf7c41973c4faa92ab70e4450b190c
Echo <edwin.aramburo1@gmail.com>
2026-02-12 16:50:52 -0500

    style login page

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,19 +1,45 @@
 import { useNavigation } from '@react-navigation/native';
-import { Button, Text, View } from "react-native";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useTheme } from '../context/ThemeContext';
 
 export const LoginScreen = () => {
-      const navigation = useNavigation();
-      const { theme }  = useTheme();
+    const navigation = useNavigation();
+    const { theme, colors, spacing, container, logo } = useTheme();
 
     return (
-        <View>
-            <Text>Login</Text>
-            <Button
-                title="Go to Home"
-                onPress={() => navigation.navigate('Home')}
-                color={theme.colors.primary.main}
+        <View style={[container.pageContainer, container.loginPageContainer]} id='login-page-container'>
+            <View style={container.logoContainer} id='logo-container'>
+                <Image
+                    style={logo}
+                    source={require("../../assets/images/splash-icon.png")}
+                />
+            </View>
+            <Text style={{fontWeight: 'bold', color: 'white', textAlign: 'center', fontSize: 24}}>Let's Start Cooking!</Text>
+            <TextInput
+                style={container.inputContainer}
+                placeholder="Username"
+            // value={user.username}
+            // onChangeText={(username) => setUser({ ...user, username })}
             />
+            <TextInput
+                style={container.inputContainer}
+                placeholder="Password"
+            // value={user.password}
+            // onChangeText={(password) => setUser({ ...user, password })}
+            />
+
+            {/* {state.error && (
+                <Text style={formStyles.errorText}>{state.error}</Text>
+            )} */}
+
+            <TouchableOpacity
+                style={[container.buttonContainer, container.buttonAlt]}
+                // onPress={handleLogin}
+                // disabled={state.isLoading}
+                onPress={() => navigation.navigate('Home')}
+            >
+                <Text style={{ textAlign: 'center', fontWeight: 'bold', color: 'white' }}>LOGIN</Text>
+            </TouchableOpacity>
         </View>
     )
 
